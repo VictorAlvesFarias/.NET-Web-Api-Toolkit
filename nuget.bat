395e3d3e1723eaf3a1beb65b8b1eb2be95b39706
Victor Alves Faria <victoralvesfarias2004@gmail.com>
2025-10-26 21:25:13 -0300

    Update

---
+++ nuget.bat
@@ -0,0 +1,5 @@
+dotnet nuget add source "https://nuget.pkg.github.com/VictorAlvesFarias/index.json" \
+  --name "GitHubPackages" \
+  --username "VictorAlvesFarias" \
+  --password "<TOKEN>" \
+  --store-password-in-clear-text
